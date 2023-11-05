9a5d4b86c9c6951c6c7382182282f2b615866d93
jonatan <jsegovia.ush@gmail.com>
2023-11-05 10:42:04 -0300

    refactor (getData): function modified to adapt new DB

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,9 +1,8 @@
-export const getData = async () => {
+/* export const getData = async () => {
   try {
     const response = await fetch(
       'https://asker-83340-default-rtdb.firebaseio.com/data.json'
     );
-
     if (!response.ok) {
       throw new Error('Erro when obtaining data');
     }
@@ -17,6 +16,24 @@ export const getData = async () => {
     return data;
   } catch (error) {
     console.error('There was an error:', error);
+    throw error;
+  }
+};
+ */
+import { collection, getDocs, getFirestore } from 'firebase/firestore/lite';
+
+const FirebaseDB = getFirestore();
+
+export const getData = async () => {
+  try {
+    const colRef = collection(FirebaseDB, 'data');
+    const snapshot = await getDocs(colRef);
+    const data = snapshot.docs.map((doc) => doc.data());
+
+    return data;
+  } catch (error) {
+    console.error('There was an error:', error);
+
     throw error;
   }
 };
