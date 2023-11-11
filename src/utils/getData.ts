ac751242e45b5aa08eba4c46a652dc708614439c
jonatan <jsegovia.ush@gmail.com>
2023-11-11 09:11:38 -0300

    refactor (getData): mocked data deleted, types added

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,34 +1,23 @@
-/* export const getData = async () => {
-  try {
-    const response = await fetch(
-      'https://asker-83340-default-rtdb.firebaseio.com/data.json'
-    );
-    if (!response.ok) {
-      throw new Error('Erro when obtaining data');
-    }
+import {
+  DocumentData,
+  collection,
+  getDocs,
+  getFirestore,
+} from 'firebase/firestore/lite';
 
-    const data = await response.json();
-
-    if (!data || typeof data !== 'object') {
-      throw new Error('Unexpected format');
-    }
-
-    return data;
-  } catch (error) {
-    console.error('There was an error:', error);
-    throw error;
-  }
-};
- */
-import { collection, getDocs, getFirestore } from 'firebase/firestore/lite';
-
-const FirebaseDB = getFirestore();
+export const FirebaseDB = getFirestore();
 
 export const getData = async () => {
   try {
     const colRef = collection(FirebaseDB, 'data');
-    const snapshot = await getDocs(colRef);
-    const data = snapshot.docs.map((doc) => doc.data());
+
+    const snapshot = await getDocs(colRef)
+      .then((collection) => collection)
+      .catch((e) => console.log(e));
+
+    const data: DocumentData[] | undefined = snapshot?.docs.map((doc) =>
+      doc.data()
+    );
 
     return data;
   } catch (error) {
