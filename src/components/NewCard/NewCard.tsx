ef5827f909ac24cfd778648ab1640913095ce942
jonatan <jsegovia.ush@gmail.com>
2023-11-12 11:16:39 -0300

    feature (NewCard): css file created, logic added

--- src/components/NewCard/NewCard.tsx
+++ src/components/NewCard/NewCard.tsx
@@ -1,5 +1,38 @@
+import { ChangeEvent, useState } from 'react';
+import NewItem from '../NewItem/NewItem';
+
+import addData from '../../utils/addData';
+import Button from '../Button/Button';
+
+import './NewCard.css';
+
 const NewCard = () => {
-  return <div>NewCard</div>;
+  const [frontValue, setValueFront] = useState('');
+  const [backValue, setValueBack] = useState('');
+
+  const handleChangeFront = (e: ChangeEvent<HTMLDivElement>) =>
+    setValueFront(e.currentTarget.textContent || '');
+
+  const handleChangeBack = (e: ChangeEvent<HTMLDivElement>) =>
+    setValueBack(e.currentTarget.textContent || '');
+
+  const handleSubmit = () => {
+    if (!frontValue || !backValue)
+      return alert('You should add values in both cards!');
+
+    confirm('Are you sure?') &&
+      frontValue &&
+      backValue &&
+      addData({ frontValue, backValue });
+  };
+
+  return (
+    <main className="container-newitems">
+      <NewItem handleChange={handleChangeFront} name="front-new" text="Front" />
+      <NewItem handleChange={handleChangeBack} name="back-new" text="Back" />
+      <Button handleClick={handleSubmit} message="Submit" />
+    </main>
+  );
 };
 
 export default NewCard;
