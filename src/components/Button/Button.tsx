8ed294b0822515c41c5ce85c4ec100fd9a4347ed
jonatan.segovia <jsegovia.ush@gmail.com>
2023-11-25 19:52:56 -0300

    refactor (Button):  isEditing deleted, iconName added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,13 +6,13 @@ import './Button.css';
 
 interface ButtonProps {
   handleClick: MouseEventHandler<HTMLButtonElement>;
-  isEditing?: boolean;
+  iconName?: string;
   message?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ handleClick, isEditing, message }) => (
+const Button: React.FC<ButtonProps> = ({ handleClick, iconName, message }) => (
   <button className="button" onClick={handleClick}>
-    {message ? message : <Icon editing={isEditing} />}
+    {message ? message : <Icon iconName={iconName} />}
   </button>
 );
 
