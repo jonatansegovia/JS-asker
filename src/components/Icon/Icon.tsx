61e492f1dc0787e4803c395f9406c8026d32546e
jonatan.segovia <jsegovia.ush@gmail.com>
2023-11-26 09:34:39 -0300

    feature (Icon): editMode controller added

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,11 +1,28 @@
+import { useContext, MouseEvent } from 'react';
+
+import { Context } from '../../provider/Context';
+
 import './Icon.styled.css';
 
 interface IconProps {
   iconName?: string;
 }
 
-const Icon = ({ iconName }: IconProps) => (
-  <span className="material-symbols-outlined">{iconName}</span>
-);
+const Icon = ({ iconName }: IconProps) => {
+  const { setEditMode, editMode } = useContext(Context);
+
+  const handleClick = (e: MouseEvent<HTMLSpanElement>) => {
+    if (iconName !== 'edit_square') return;
+
+    e.stopPropagation();
+    setEditMode(!editMode);
+  };
+
+  return (
+    <span className="material-symbols-outlined" onClick={(e) => handleClick(e)}>
+      {iconName}
+    </span>
+  );
+};
 
 export default Icon;
