401a3760aa1204761e734792069b8c7ce533a678
jonatan.segovia <jsegovia.ush@gmail.com>
2023-11-26 09:35:45 -0300

    refactor (Item): unnecesary condition data deleted

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,20 +1,20 @@
 import { DocumentData } from 'firebase/firestore/lite';
 
+import Icon from '../Icon/Icon';
+
 import './Item.styled.css';
 
 interface Item {
   isBack?: boolean;
-  data: DocumentData | undefined;
+  data?: DocumentData | undefined;
   name: 'front' | 'back';
   handleClick?: () => void;
 }
 
 const Item = ({ isBack, data, name, handleClick }: Item) => (
   <div className={name} onClick={handleClick}>
-    {data && (
-      <p className="inner-text">{isBack ? data.answer : data.question}</p>
-    )}
-    <span className="material-symbols-outlined">edit_square</span>
+    <p className="inner-text">{isBack ? data?.answer : data?.question}</p>
+    <Icon iconName="edit_square" />
   </div>
 );
 
