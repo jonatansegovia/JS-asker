70f5eeadb092cbc492edd7f896d4919a5b9fef3b
jonatan.segovia <jsegovia.ush@gmail.com>
2023-11-29 10:10:58 -0300

    refactor (updateData): db data needed added

--- src/utils/updateData.ts
+++ src/utils/updateData.ts
@@ -2,16 +2,7 @@ import { doc, updateDoc } from 'firebase/firestore/lite';
 
 import { FirebaseDB } from './getData';
 
-interface UpdateDataParams {
-  id: string;
-  newData: {
-    answer?: string;
-    question?: string;
-  };
-}
-
-export const updateData = ({ id, newData }: UpdateDataParams) => {
-  const cardRef = doc(FirebaseDB, 'qaCollection', id);
-  console.log(cardRef);
-  updateDoc(cardRef, newData).then(() => console.log('DONE'));
+export const updateData = (id: string, newData: { [x: string]: string }) => {
+  const cardDBRef = doc(FirebaseDB, 'qaCollection', id);
+  updateDoc(cardDBRef, newData).then(() => console.log('DONE'));
 };
