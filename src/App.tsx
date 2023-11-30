186d5d4a7bff2c957f71c2b196b09fbc5af9bdc8
jonatan.segovia <jsegovia.ush@gmail.com>
2023-11-30 10:32:34 -0300

    refactor (App): dark-theme added, btn for dark added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 
+import { Context } from './provider/Context';
 import Button from './components/Button/Button';
 import Card from './components/Card/Card';
 import NewCard from './components/NewCard/NewCard';
@@ -8,15 +9,23 @@ import './App.css';
 
 function App() {
   const [createMode, setCreateMode] = useState(false);
+  const { isDark, setTheme } = useContext(Context);
 
   const handleClick = () => setCreateMode(!createMode);
 
+  console.log('isdark', isDark);
   return (
-    <div id="main">
-      <Button
-        handleClick={handleClick}
-        iconName={!createMode ? 'credit_card_gear' : 'keyboard_backspace'}
-      />
+    <div id="main" className={isDark ? 'dark-theme' : undefined}>
+      <header className="btn-container">
+        <Button
+          handleClick={handleClick}
+          iconName={!createMode ? 'credit_card_gear' : 'keyboard_backspace'}
+        />
+        <Button
+          handleClick={() => setTheme(!isDark)}
+          iconName={isDark ? 'clear_night' : 'wb_sunny'}
+        />
+      </header>
       {!createMode ? <Card /> : <NewCard />}
     </div>
   );
