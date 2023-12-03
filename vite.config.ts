263b4de0dcee09412f97947faace42fd3314e602
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-02 21:19:44 -0300

    refactor (vite.config): test config added

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: { environment: 'jsdom' },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
 });
