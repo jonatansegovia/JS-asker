2c983f5f6c9873f0ef2245f0faea2d181e05000c
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-02 21:21:06 -0300

    test (App.test): render correctly working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,21 @@
 import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 
 import './firebase/config.js';
 
 import App from './App';
 
 describe('<App/>', () => {
-  it('should render correctly', () => {
+  it('should render correctly', async () => {
     render(<App />);
+
+    await waitFor(
+      async () => {
+        const card = await screen.findByTestId('inner-text');
+
+        expect(card).toBeInTheDocument();
+      },
+      { timeout: 2000 }
+    );
   });
 });
