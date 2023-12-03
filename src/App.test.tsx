f24076a7e51f8de561535918ea9af06b183b9cf4
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-03 08:47:11 -0300

    test (App): edit mode changing checked

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,13 @@
 import { describe, it } from 'vitest';
 import { render, waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import './firebase/config.js';
 
 import App from './App';
 
 describe('<App/>', () => {
-  let container;
+  let container: HTMLElement;
 
   beforeEach(() => {
     container = render(<App />).container;
@@ -22,6 +23,25 @@ describe('<App/>', () => {
         expect(editBtn).toBeInTheDocument();
         expect(themeBtn).toBeInTheDocument();
         expect(card).toBeInTheDocument();
+      },
+      { timeout: 2500 }
+    );
+  });
+
+  it('should toggle create mode on create button click', async () => {
+    await waitFor(
+      async () => {
+        const editBtn = await screen.findByText('credit_card_gear');
+
+        await userEvent.click(editBtn);
+
+        const backBtn = await screen.findByText('keyboard_backspace');
+        const containerNewItems = container.querySelector(
+          '.container-newitems'
+        );
+
+        expect(backBtn).toBeInTheDocument();
+        expect(containerNewItems).toBeInTheDocument();
       },
       { timeout: 2500 }
     );
