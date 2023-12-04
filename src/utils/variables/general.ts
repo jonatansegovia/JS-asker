f40ddb284579f3b6b15032fe3483e12f8d9a5faa
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-03 23:56:49 -0300

    feature (general): utils for general variables names added

---
+++ src/utils/variables/general.ts
@@ -0,0 +1,7 @@
+export const DARK_THEME: string = 'dark-theme';
+export const EDIT_GEAR: string = 'credit_card_gear';
+export const EDIT_PENCIL: string = 'edit_square';
+export const LEFT_ARROW: string = 'keyboard_backspace';
+export const MOON: string = 'clear_night';
+export const SAVE: string = 'save';
+export const SUN: string = 'wb_sunny';
