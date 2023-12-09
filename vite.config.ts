3623ef4365a8572bff08c0d088d817580bb76e05
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-09 12:46:24 -0300

    feature (several): vitest config added

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test/setup.ts'],
-    coverage: {
-      provider: 'istanbul',
-      reportsDirectory: './tests/unit/coverage',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.{ts,tsx}', '!src/types.ts'],
-      all: true,
-      enabled: true,
-    },
-  },
 });
---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest"/>
+
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test/setup.ts'],
+    dangerouslyIgnoreUnhandledErrors: true,
+    testTransformMode: { ssr: ['src/**/*.{ts,tsx}'] },
+    coverage: {
+      provider: 'istanbul',
+      reportsDirectory: './tests/unit/coverage',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{ts,tsx}', '!src/types.ts'],
+      all: true,
+      enabled: true,
+    },
+  },
+});
