6984a3cd5e4b714a9c568f2bbbfa6af914d33c7d
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-10 08:26:26 -0300

    refactor (Item): paragraph replaced for InnerText component

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,18 +1,17 @@
 import { DocumentData } from 'firebase/firestore/lite';
 
 import EditArea from '../EditArea/EditArea';
-import Icon from '../Icon/Icon';
-import { EDIT_PENCIL } from '../../utils/variables/general';
+import InnerText from '../InnerText/InnerText';
 import './Item.styled.css';
 
 interface ItemProps {
+  data: DocumentData | undefined;
   edit: boolean;
   handleClick?: () => void;
   name: string;
-  showAnswer: boolean;
   setData: (text: DocumentData) => void;
   setEditMode: (mode: boolean) => void;
-  data: DocumentData | undefined;
+  showAnswer: boolean;
 }
 
 const Item = ({
@@ -33,17 +32,12 @@ const Item = ({
         cardData={data}
       />
     ) : (
-      <>
-        <p
-          className="inner-text"
-          contentEditable={edit}
-          data-testid="inner-text"
-          suppressContentEditableWarning
-        >
-          {showAnswer ? data?.answer : data?.question}
-        </p>
-        <Icon iconName={EDIT_PENCIL} handleClick={() => setEditMode(!edit)} />
-      </>
+      <InnerText
+        edit={edit}
+        data={data}
+        setEditMode={setEditMode}
+        showAnswer={showAnswer}
+      />
     )}
   </div>
 );
