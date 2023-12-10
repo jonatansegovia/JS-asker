737ebcf1f05038334b88416c8afc46589eededca
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-10 07:53:13 -0300

    test (NewItem): card writing

--- src/components/NewItem/NewItem.test.tsx
+++ src/components/NewItem/NewItem.test.tsx
@@ -1,16 +1,50 @@
 import { describe, it, expect, vi } from 'vitest';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import NewItem from './NewItem';
+import userEvent from '@testing-library/user-event';
+
+const testHandleClick = vi.fn();
+const testingLabel = 'TEST FRONT';
+const testingName = 'test-name';
 
 describe('first', () => {
-  it('should ', () => {
+  it('should render correctly', () => {
     const { container } = render(
-      <NewItem handleChange={vi.fn()} name="test-name" text="TEST FRONT" />
+      <NewItem
+        handleChange={testHandleClick}
+        name={testingName}
+        text={testingLabel}
+      />
     );
 
     const label = container.querySelector('label[for="test-name"]');
+    const card = container.querySelector('.test-name');
 
     expect(label).toBeInTheDocument();
+    expect(label?.innerHTML).toBe(testingLabel);
+    expect(label).toHaveAttribute('for', 'test-name');
+    expect(card).toBeInTheDocument();
+    expect(card).toHaveClass('test-name');
+    expect(card).toHaveAttribute('contenteditable', 'true');
+  });
+
+  it('should we clickable', async () => {
+    const { container } = render(
+      <NewItem
+        handleChange={testHandleClick}
+        name={testingName}
+        text={testingLabel}
+      />
+    );
+
+    const someText = 'some text';
+
+    const card = container.querySelector('.test-name') as Element;
+
+    await userEvent.click(card);
+    await userEvent.type(card, someText);
+    screen.debug();
+    expect(testHandleClick).toHaveBeenCalledTimes(someText.length);
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -10,13 +10,13 @@ export default defineConfig({
     setupFiles: ['src/test/setup.ts'],
     dangerouslyIgnoreUnhandledErrors: true,
     testTransformMode: { ssr: ['src/**/*.{ts,tsx}'] },
-    coverage: {
-      provider: 'istanbul',
-      reportsDirectory: './tests/unit/coverage',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.{ts,tsx}', '!src/types.ts'],
-      all: true,
-      enabled: true,
-    },
+    //coverage: {
+    //  provider: 'istanbul',
+    //  reportsDirectory: './tests/unit/coverage',
+    //  reporter: ['text', 'json', 'html'],
+    //  include: ['src/**/*.{ts,tsx}', '!src/types.ts'],
+    //  all: true,
+    //  enabled: true,
+    // },
   },
 });
