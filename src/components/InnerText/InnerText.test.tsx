82c349a6e894a5efc171d37d51506ab7fcba6059
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-11 09:50:41 -0300

    test (InnerText): render question, answer and call fn

--- src/components/InnerText/InnerText.test.tsx
+++ src/components/InnerText/InnerText.test.tsx
@@ -3,16 +3,20 @@ import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import InnerText from './InnerText';
+import userEvent from '@testing-library/user-event';
 
 const testingSetEditMode = vi.fn();
-const testingFront: DocumentData = { answer: 'testing front' };
+const testingData: DocumentData = {
+  answer: 'testing front',
+  question: 'testing question',
+};
 
 describe('<InnerText />', () => {
   it('should render correctly', () => {
     const { container } = render(
       <InnerText
         edit={false}
-        data={testingFront}
+        data={testingData}
         setEditMode={testingSetEditMode}
         showAnswer={false}
       />
@@ -23,5 +27,54 @@ describe('<InnerText />', () => {
 
     expect(frontCard).toBeInTheDocument();
     expect(icon).toBeInTheDocument();
+  });
+
+  it('should render question', () => {
+    render(
+      <InnerText
+        edit={false}
+        data={testingData}
+        setEditMode={testingSetEditMode}
+        showAnswer={false}
+      />
+    );
+
+    const frontCard = screen.getByTestId('inner-text');
+
+    expect(frontCard.innerHTML).toBe(testingData.question);
+  });
+
+  it('should render answer', () => {
+    render(
+      <InnerText
+        edit={false}
+        data={testingData}
+        setEditMode={testingSetEditMode}
+        showAnswer={true}
+      />
+    );
+
+    const frontCard = screen.getByTestId('inner-text');
+
+    expect(frontCard.innerHTML).toBe(testingData.answer);
+  });
+
+  it('should call fn when click on icon', async () => {
+    const { container } = render(
+      <InnerText
+        edit={false}
+        data={testingData}
+        setEditMode={testingSetEditMode}
+        showAnswer={true}
+      />
+    );
+
+    const icon = container.querySelector(
+      '[class="material-symbols-outlined"]'
+    ) as Element;
+
+    await userEvent.click(icon);
+
+    expect(testingSetEditMode).toHaveBeenCalledOnce();
   });
 });
