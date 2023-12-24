0794218f2068436a7967743dd4663f05bfed195b
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-24 13:02:41 -0300

    refactor (NewCard): Banner addeed and logic passed to addData

--- src/components/NewCard/NewCard.tsx
+++ src/components/NewCard/NewCard.tsx
@@ -1,33 +1,44 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import NewItem from '../NewItem/NewItem';
 
 import addData from '../../utils/addData';
 import Button from '../Button/Button';
+import { Context } from '../../provider/Context';
 
 import './NewCard.css';
+import Banner from '../Banner/Banner';
 
 const NewCard = () => {
   const [frontValue, setValueFront] = useState('');
   const [backValue, setValueBack] = useState('');
+  const [status, setStatus] = useState('');
+  const { bannerVisible, setBannerVisible } = useContext(Context);
+
+  useEffect(() => {
+    if (status === 'success') {
+      setValueBack('');
+      setValueFront('');
+    }
+  }, [status]);
 
   const handleChangeFront = (e: ChangeEvent<HTMLTextAreaElement>) =>
     setValueFront(e.currentTarget.value);
   const handleChangeBack = (e: ChangeEvent<HTMLTextAreaElement>) =>
     setValueBack(e.currentTarget.value);
+
   const handleSubmit = () => {
     if (!frontValue || !backValue) {
       return alert('You should add values in both cards!');
     }
 
     if (frontValue && backValue && confirm('Are you sure?')) {
-      addData({ frontValue, backValue });
-      setValueBack('');
-      setValueFront('');
+      addData({ frontValue, backValue, setBannerVisible, setStatus });
     }
   };
 
   return (
     <main className="container-newitems">
+      <Banner type={status} visible={bannerVisible} />
       <NewItem
         handleChange={handleChangeFront}
         name="front-new"
