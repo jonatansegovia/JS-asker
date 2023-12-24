71046e95eb8f5862efb480a4ec2d9cfe2bcf50f9
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-24 10:52:33 -0300

    refactor (NewItem): div changed to textarea, value passed

--- src/components/NewItem/NewItem.tsx
+++ src/components/NewItem/NewItem.tsx
@@ -4,17 +4,16 @@ import './NewItem.css';
 
 interface NewItem {
   name: string;
-  handleChange: (e: ChangeEvent<HTMLDivElement>) => void;
+  handleChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
   text: string;
+  value: string;
 }
 
-const NewItem = ({ handleChange, name, text }: NewItem) => {
-  return (
-    <>
-      <label htmlFor={name}>{text}</label>
-      <div className={name} contentEditable onInput={handleChange}></div>
-    </>
-  );
-};
+const NewItem = ({ handleChange, name, text, value }: NewItem) => (
+  <>
+    <label htmlFor={name}>{text}</label>
+    <textarea className={name} onChange={handleChange} value={value}></textarea>
+  </>
+);
 
 export default NewItem;
