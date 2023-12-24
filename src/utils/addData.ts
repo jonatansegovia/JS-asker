e054980e08eb53023ac5ed1103f23361ee41ea49
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-24 13:02:12 -0300

    refactor (addData): logic to control banner added

--- src/utils/addData.ts
+++ src/utils/addData.ts
@@ -5,13 +5,28 @@ import { FirebaseDB } from './getData';
 interface Data {
   frontValue: string;
   backValue: string;
+  setBannerVisible: (visible: boolean) => void;
+  setStatus: (status: 'success' | 'error') => void;
 }
 
-const addData = ({ frontValue, backValue }: Data) => {
+const addData = ({
+  frontValue,
+  backValue,
+  setBannerVisible,
+  setStatus,
+}: Data) => {
   const collectionRef = collection(FirebaseDB, 'data');
   const quiz = { [frontValue]: backValue };
 
-  addDoc(collectionRef, quiz).then(() => alert('Added!'));
+  addDoc(collectionRef, quiz)
+    .then(() => {
+      setBannerVisible(true);
+      setStatus('success');
+    })
+    .catch(() => {
+      setBannerVisible(true);
+      setStatus('error');
+    });
 };
 
 export default addData;
