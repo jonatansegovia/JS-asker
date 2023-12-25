bfa7135af82489c41709662d44f785fe4f3cb713
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-25 11:25:10 -0300

    feature (updateData): setBannerVisible and setStatus added

--- src/utils/updateData.ts
+++ src/utils/updateData.ts
@@ -2,7 +2,20 @@ import { doc, updateDoc } from 'firebase/firestore/lite';
 
 import { FirebaseDB } from './getData';
 
-export const updateData = (id: string, newData: { [x: string]: string }) => {
+export const updateData = (
+  id: string,
+  newData: { [x: string]: string },
+  setBannerVisible: (visible: boolean) => void,
+  setStatus: (status: 'success' | 'error') => void
+) => {
   const cardDBRef = doc(FirebaseDB, 'qaCollection', id);
-  updateDoc(cardDBRef, newData).then(() => console.log('DONE'));
+  updateDoc(cardDBRef, newData)
+    .then(() => {
+      setBannerVisible(true);
+      setStatus('success');
+    })
+    .catch(() => {
+      setBannerVisible(true);
+      setStatus('error');
+    });
 };
