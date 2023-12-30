ba10241c9aa27ecb60463f7e1a3d4d281102add3
jonatan.segovia <jsegovia.ush@gmail.com>
2023-12-29 22:27:54 -0300

    test (Icon): should render correct icon and class when adding one

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -8,16 +8,23 @@ import Icon from './Icon';
 
 describe('<Icon />', () => {
   const testHandleClick = vi.fn();
-  const defaultClass = 'material-symbols-outlined save';
+  const defaultClass = 'material-symbols-outlined';
 
   it('should render correctly', () => {
     render(<Icon handleClick={testHandleClick} />);
 
     const saveIcon = screen.getByText(SAVE);
 
-    screen.debug();
-
     expect(saveIcon).toBeInTheDocument();
-    expect(saveIcon.className).toBe(defaultClass);
+    expect(saveIcon.className).toBe(`${defaultClass} ${SAVE}`);
+  });
+
+  it('should render correct icon and class when adding one', () => {
+    render(<Icon handleClick={testHandleClick} iconName={EDIT_PENCIL} />);
+
+    const pencilIcon = screen.getByText(EDIT_PENCIL);
+
+    expect(pencilIcon).toBeInTheDocument();
+    expect(pencilIcon.className).toBe(defaultClass);
   });
 });
