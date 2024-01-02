e96941237cd17ad2b925e1664be4c4793b01d58c
jonatan.segovia <jsegovia.ush@gmail.com>
2024-01-02 20:30:21 -0300

    feature (mockedData): mocked data db added

---
+++ src/utils/mockedData.js
@@ -0,0 +1,13 @@
+const data = [
+  { id: 'test1', answer: 'testing answer 1', question: 'testing question 1' },
+  { id: 'test2', answer: 'testing answer 2', question: 'testing question 2' },
+  { id: 'test3', answer: 'testing answer 3', question: 'testing question 3' },
+  { id: 'test4', answer: 'testing answer 4', question: 'testing question 4' },
+  { id: 'test5', answer: 'testing answer 5', question: 'testing question 5' },
+  { id: 'test6', answer: 'testing answer 6', question: 'testing question 6' },
+  { id: 'test7', answer: 'testing answer 7', question: 'testing question 7' },
+  { id: 'test8', answer: 'testing answer 8', question: 'testing question 8' },
+  { id: 'test9', answer: 'testing answer 9', question: 'testing question 9' },
+];
+
+export default data;
